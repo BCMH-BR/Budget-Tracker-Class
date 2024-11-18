d2ee89d59450f0fb8da7a798811f6ef4e0d5c50a
chryswds <dybas.chrys@gmail.com>
2024-11-18 15:59:24 +0000

    changing variable type

--- src/connection/database.js
+++ src/connection/database.js
@@ -1,6 +1,6 @@
-var mysql = require('mysql2');
+const mysql = require('mysql2');
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
