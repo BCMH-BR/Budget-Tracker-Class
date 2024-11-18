efcd4b19b5aeb70bf76b877446c77e82967bd3f9
chryswds <dybas.chrys@gmail.com>
2024-11-18 19:26:20 +0000

    :wrench: Database is now created automatically - user doesnt need to manually create it

--- src/connection/database.js
+++ src/connection/database.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    multipleStatements: true //implementation - initial connection withou a database specified
 })
 
 module.exports = connection;
