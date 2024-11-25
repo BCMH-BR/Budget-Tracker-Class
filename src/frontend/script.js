88dd11d5976f273299c42f5623dfa387b1e048ce
chryswds <dybas.chrys@gmail.com>
2024-11-25 12:24:25 +0000

    teacher suggestions

--- src/frontend/script.js
+++ src/frontend/script.js
@@ -10,3 +10,11 @@ popupLink.addEventListener("click", function (event) {
 closeButton.addEventListener("click", function () {
   popupWindow.style.display = "none";
 });
+
+
+// Use Document Object Model DOM API
+// 
+// Instead of just one transction per click
+// create multiple transactions or just one
+// if multiple you take in all of the transactions into a JSON object or arrayList
+// and insert it into the SQL
