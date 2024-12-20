80d28ec0114c48a3316d1057087671745d343270
chryswds <dybas.chrys@gmail.com>
2024-12-20 18:58:11 +0000

    deleting comments

--- src/config/database.js
+++ src/config/database.js
@@ -69,47 +69,7 @@ function initDB() {
         );
       }
     });
-    // const createUsersTable = CREATE TABLE IF NOT EXISTS Users(user_id INT AUTO_INCREMENT PRIMARY KEY,user_name VARCHAR(255));
-    // connection.query(createUsersTable, function (err) {
-    //   if (err) {
-    //     console.error("Error creating Users table");
-    //     throw err;
-    //   } else {
-    //     console.log("Users Table Created");
-    //   }
-    // });
 
-    // const createTransactionTypeTable = CREATE TABLE IF NOT EXISTS TransactionType(type_id INT AUTO_INCREMENT PRIMARY KEY,type_name VARCHAR(255));;
-    // connection.query(createTransactionTypeTable, function (err) {
-    //   if (err) {
-    //     console.error("Error creating Transactions Type table");
-    //     throw err;
-    //   } else {
-    //     console.log("Transactions Table Created");
-    //   }
-    // });
-
-    // const createCategoryTable = CREATE TABLE IF NOT EXISTS Categories(category_id INT AUTO_INCREMENT PRIMARY KEY,category_name VARCHAR(255),type_id INT,FOREIGN KEY (type_id) REFERENCES TransactionType (type_id));;
-    // connection.query(createCategoryTable, function (err) {
-    //   if (err) {
-    //     console.error("Error creating Categories table");
-    //     throw err;
-    //   } else {
-    //     console.log("Categories Table Created");
-    //   }
-    // });
-
-    // const createWalletTable = ` CREATE TABLE IF NOT EXISTS Wallet(wallet_id INT PRIMARY KEY,wallet_name VARCHAR(255));`;
-    // connection.query(createWalletTable, function (err) {
-    //   if (err) {
-    //     console.error("Error creating Wallet table");
-    //     throw err;
-    //   } else {
-    //     console.log("Wallet Table Created");
-    //   }
-    // });
-
-    // const createTransactionsTable = CREATE TABLE IF NOT EXISTS Transactions(transaction_id INT AUTO_INCREMENT PRIMARY KEY,type_id INT,FOREIGN KEY (type_id) REFERENCES TransactionType (type_id),category_id INT,FOREIGN KEY (category_id) REFERENCES Categories (category_id),transaction_description VARCHAR(255),amount DOUBLE,transaction_date DATE,wallet_id INT,FOREIGN KEY (wallet_id) REFERENCES Wallet (wallet_id),user_id INT,FOREIGN KEY (user_id) REFERENCES Users (user_id));;
 
     const createTransactionsTable = `CREATE TABLE IF NOT EXISTS Transactions (
     transaction_id INT AUTO_INCREMENT PRIMARY KEY, 
